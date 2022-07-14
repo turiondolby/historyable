709706adcdef181ee602223821094af21fea23d9
Haleem Hosein <haleemhosein@yahoo.com>
2022-07-13 21:27:54 -0400

    06 Saving history changes

--- app/History/Traits/Historyable.php
+++ app/History/Traits/Historyable.php
@@ -12,8 +12,19 @@ trait Historyable
     public static function bootHistoryable()
     {
         static::updated(function (Model $model) {
-            dd($model->getChangedColumns($model));
+            collect($model->getChangedColumns($model))->each(function ($change) use ($model) {
+                $model->saveChange($change);
+            });
         });
+    }
+
+    protected function saveChange(ColumnChange $change)
+    {
+        $this->history()->create([
+            'changed_column' => $change->column,
+            'changed_value_from' => $change->from,
+            'changed_value_to' => $change->to
+        ]);
     }
 
     protected function getChangedColumns(Model $model)
